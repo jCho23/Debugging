f96b0e47621715e58e733d837b5efc55a469eba3
JuneCho23 <junecho.ucb@gmail.com>
2017-09-13 18:27:11 -0700

    finished CellStackWindow

--- Debugging/CallStackWindow.cs
+++ Debugging/CallStackWindow.cs
@@ -1,52 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Collections.Generic;
 
-namespace Debugging
-{
-    public class CallStackWindow
-    {
-        public static void Main(string[] args)
-        {
-            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
-            var smallests = GetSmallests(numbers, 3);
+//namespace Debugging
+//{
+//    public class CallStackWindow
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+//            var smallests = GetSmallests(numbers, 3);
 
-            foreach (var number in numbers)
-                Console.WriteLine(number);
-        }
+//            foreach (var number in numbers)
+//                Console.WriteLine(number);
+//        }
 
-        public static List<int> GetSmallests(List<int> list, int count)
-        {
-            if (count > list.Count)
-                throw new ArgumentOutOfRangeException("count", "Count cannot be greater than the number in the list");
+//        public static List<int> GetSmallests(List<int> list, int count)
+//        {
+//            if (count > list.Count)
+//                throw new ArgumentOutOfRangeException("count", "Count cannot be greater than the number in the list");
 
 
-            var buffer = new List<int>(list);
+//            var buffer = new List<int>(list);
 
-            var smallests = new List<int>();
+//            var smallests = new List<int>();
 
-            while (smallests.Count < count)
-            {
-                var min = GetSmallest(buffer);
-                smallests.Add(min);
+//            while (smallests.Count < count)
+//            {
+//                var min = GetSmallest(buffer);
+//                smallests.Add(min);
           
 
-                buffer.Remove(min);
-            }
+//                buffer.Remove(min);
+//            }
 
-            return smallests;
-        }
+//            return smallests;
+//        }
 
-        public static int GetSmallest(List<int> list)
-        {
-            var min = list[0];
-            for (var i = 1; i < list.Count; i++)
-            {
-                if (list[i] < min)
-                    min = list[i];
-            }
+//        public static int GetSmallest(List<int> list)
+//        {
+//            var min = list[0];
+//            for (var i = 1; i < list.Count; i++)
+//            {
+//                if (list[i] < min)
+//                    min = list[i];
+//            }
 
-            return min;
-        }
+//            return min;
+//        }
 
-    }
-}
+//    }
+//}
