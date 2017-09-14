0609f6ff557d3cf7facafdd27f0a331d4f4d1ac2
JuneCho23 <junecho.ucb@gmail.com>
2017-09-13 18:21:38 -0700

    CleanUp

--- Debugging/CallStackWindow.cs
+++ Debugging/CallStackWindow.cs
@@ -16,20 +16,16 @@ namespace Debugging
 
         public static List<int> GetSmallests(List<int> list, int count)
         {
-            //We are creating a variable here to do the processing of the list 
             var buffer = new List<int>(list);
 
             var smallests = new List<int>();
 
             while (smallests.Count < count)
             {
-                //Here, we are passing in the buffer instead of the list
-                //var min = GetSmallest(list);
                 var min = GetSmallest(buffer);
                 smallests.Add(min);
-                //Instead of removing the objects here from the original list,
-                //we are going to do it in the buffer
-                //list.Remove(min);
+          
+
                 buffer.Remove(min);
             }
 
@@ -38,7 +34,6 @@ namespace Debugging
 
         public static int GetSmallest(List<int> list)
         {
-            //Assume the first number is the smallest
             var min = list[0];
             for (var i = 1; i < list.Count; i++)
             {
